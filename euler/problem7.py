9ab62b0724478cfe8aec54db4f4d8f7a15bce09d
Ivan Idris <ivan.idris@gmail.com>
2012-02-25 13:42:44 +0100

    Added problem 7 solution.

---
+++ euler/problem7.py
@@ -0,0 +1,29 @@
+import numpy
+
+LIM = 10 ** 6
+N = 10 ** 9
+P = 10001
+primes = []
+p = 2
+
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+#What is the 10 001st prime number?
+
+def check_primes(a, p):
+   #2. Sieve out multiples of p
+   a = a[a % p != 0]
+
+   return a
+
+for i in xrange(3, N, LIM):
+   #1. Create a list of consecutive integers
+   a = numpy.arange(i, i + LIM, 2)
+
+   while len(primes) < P:
+      a = check_primes(a, p)
+      primes.append(p)
+
+      p = a[0]
+
+print len(primes), primes[P-1]
